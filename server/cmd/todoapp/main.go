7f965e757bc1a4ca17e7d529f7bef8a55a7b4856
Hesam Zandian <hesam.zandian@yahoo.com>
2023-06-28 11:56:12 +0330

    feat: database config and apis

--- server/cmd/todoapp/main.go
+++ server/cmd/todoapp/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"server/config"
+	"server/internal/handlers"
+	"server/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	var db = config.ConnectDB()
+	db.AutoMigrate(&models.Todo{})
+
 	routes := gin.Default()
+
+	apiRoutes := routes.Group("/api")
+
+	apiRoutes.GET("/todos", handlers.GetTodos)
+	apiRoutes.POST("/todos", handlers.CreateTodo)
 
 	err := routes.Run(":8080")
 	if err != nil {
